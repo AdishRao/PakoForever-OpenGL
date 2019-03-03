fecdcaf39bcd12590f84f302b961f447512d0ff3
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2019-03-03 19:02:28 +0530

    Add obstacle function and color management
    - color management will be useful later on to track colors in the project
    - just implemented for obstacle function currently

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#include <stdlib.h>
 
 #ifdef __linux__
 #include <GL/glut.h>
@@ -10,6 +12,52 @@
 #endif
 
 using namespace std;
+
+namespace colors
+{
+    enum colorNames {
+        BLACK=0, 
+        RED=1, 
+        GREEN=2, 
+        BLUE=3, 
+        CYAN=4, 
+        YELLOW=5, 
+        PINK=6, 
+        WHITE=7
+    };
+}
+
+GLfloat color[3] = {0, 0, 0};
+
+// sets global color vector 
+void colorMeSilly(colors::colorNames name){
+    color[0] = color[1] = color[2] = 0;
+    switch (name) {
+        case colors::BLACK: break;
+        case colors::RED: 
+            color[0] = 1;
+            break;
+        case colors::GREEN:
+            color[1] = 1;
+            break;
+        case colors::BLUE:
+            color[2] = 1;
+            break;
+        case colors::YELLOW:
+            color[0] = color[1] = 1;
+            break; 
+        case colors::CYAN:
+            color[1] = color[2] = 1;
+            break;
+        case colors::PINK:
+            color[0] = color[2] = 1;
+            break;
+        case colors::WHITE:
+            color[0] = color[1] = color[2] = 1;
+    }
+    cout<<color[0]<<color[1]<<color[2]<<endl;
+    glColor3fv(color);
+}; 
 
 float change_x=0; // 0 = left , 1 = right
 float change_y=0; // 0 = bottom, 1 = top
@@ -37,6 +85,31 @@ void keypressed(unsigned char key, int x, int y)
     cout<<key<<endl;
 }
 
+// Draws 9 sided polygons in random locations using translate
+// Polygon formula taken from  https://gist.github.com/kenpower/3782654 and modified because it drew one side less
+void drawObstacles(int obstacleCount, double radius, colors::colorNames color) {
+    // initialize random seed - otherwise random positions generated each time when display calls. 
+    srand (33);
+    // generates M sided polygon
+    const int sides=9;
+    // defining bounds for ortho
+    const int xMin=0, xMax=100, yMin=0, yMax=100;
+
+    for (int i=0; i<obstacleCount; i++){
+        // defining bounds used to limit centers of polygon 
+        const int xCenter = rand() % xMax + xMin; 
+        const int yCenter = rand() % yMax + yMin;
+
+        glBegin(GL_POLYGON);
+        for(int i=0;i<=sides;i++){
+            double angle=i*2*M_PI/sides;
+            colorMeSilly(color); // using instead of glColor
+            glVertex2d(xCenter+radius*cos(angle),yCenter+radius*sin(angle));	
+        }
+        glEnd();
+    }
+
+}
 void drawcar()
 {
     glColor3f(1.0,0.0,0.0);
@@ -81,6 +154,8 @@ void display()
     glTranslatef(change_x,change_y,0);
     drawcar();
     glPopMatrix();
+    // obstacleCount, radius, color
+    drawObstacles(7,3, colors::PINK);
     glFlush();
     glutSwapBuffers();
 }
